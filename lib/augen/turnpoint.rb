c20d2cbf3541a2a890d1da0c7df2186a447d9fca
Bruno Bonamin <bruno@bonamin.org>
2016-10-12 16:01:36 -0300

    Add Turnpoint class

---
+++ lib/augen/turnpoint.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+module Augen
+  class Turnpoint
+    attr_reader :type, :category, :length, :waypoint
+    VALID_TYPES = %i(start turnpoint finish).freeze
+    VALID_CATEGORIES = %i(
+      start_line
+      turn_point_cylinder
+      finish_cylinder
+    ).freeze
+
+    def initialize(opts = {})
+      init_type opts[:type]
+      init_category opts[:category]
+
+      @length = opts[:length] || raise(ArgumentError, 'length is needed')
+      @waypoint = opts[:waypoint] || raise(ArgumentError, 'waypoint is needed')
+    end
+
+    private
+
+    def init_type(type)
+      raise(ArgumentError, 'type is needed') if type.nil?
+      raise ArgumentError, 'invalid type' unless VALID_TYPES.include?(type)
+      @type = type
+    end
+
+    def init_category(category)
+      raise(ArgumentError, 'category is needed') if category.nil?
+      raise ArgumentError, 'invalid category' \
+        unless VALID_CATEGORIES.include?(category)
+      @category = category
+    end
+  end
+end
---
+++ spec/augen/turnpoint_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+require 'augen/waypoint'
+require 'augen/turnpoint'
+
+RSpec.describe Augen::Turnpoint do
+  let(:waypoint) do
+    Augen::Waypoint.new(
+      name: 'Rosario Apt',
+      code: 'SAAR',
+      country: 'AR',
+      latitude: '3254.150S',
+      longitude: '06047.117W',
+      elevation: '0.0m'
+    )
+  end
+  subject do
+    described_class.new(
+      type: :start,
+      category: :start_line,
+      length: 10_000,
+      waypoint: waypoint
+    )
+  end
+
+  %w(type category length waypoint).each do |attr|
+    it "has a #{attr} reader" do
+      expect(subject).to respond_to(attr)
+    end
+  end
+
+  describe '#type' do
+    it 'only accepts valid values as type' do
+      %i(start finish turnpoint).each do |type|
+        expect do
+          described_class.new(
+            type: type,
+            category: :start_line,
+            length: 10_000,
+            waypoint: waypoint
+          )
+        end.to_not raise_error
+      end
+    end
+
+    it 'does not accept random values as type' do
+      %i(foo bar a1 trap).each do |type|
+        expect do
+          described_class.new(
+            type: type,
+            category: :start_line,
+            length: 10_000,
+            waypoint: waypoint
+          )
+        end.to raise_error(ArgumentError, 'invalid type')
+      end
+    end
+  end
+  describe '#category' do
+    it 'only accepts valid values as category' do
+      %i(
+        fai_start_quadrant
+        start_line
+        start_cylinder
+        bga_start_sector
+        fai_quadrant
+        keyhole_sector
+        bga_fixed_course_sector
+        bga_enhanced_option_fixed_course_sector
+        turn_point_cylinder
+        fai_finish_quadrant
+        finish_line
+        finish_cylinder
+        symmetric_quadrant
+      ).each do |category|
+        expect do
+          described_class.new(
+            type: :start,
+            category: category,
+            length: 500,
+            waypoint: waypoint
+          )
+        end.to_not raise_error
+      end
+    end
+
+    it 'does not accept random values as category' do
+      %i(foo bar a1 trap).each do |category|
+        expect do
+          described_class.new(
+            type: :start,
+            category: category,
+            length: 10_000,
+            waypoint: waypoint
+          )
+        end.to raise_error(ArgumentError, 'invalid category')
+      end
+    end
+  end
+end
